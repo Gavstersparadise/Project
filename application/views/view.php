d90fe639e186a81040bb19c3e5626678e308bef4
Gavstersparadise <gavin_murphy1@hotmail.com>
2015-04-07 01:22:32 +0100

    Pointless facade but will be made into something meaningful probably hopefullymaybe

--- application/views/view.php
+++ application/views/view.php
@@ -54,3 +54,35 @@ $reverse_direction = ($direction == 'DESC' ? 'ASC' : 'DESC');
 		?>
 	</tbody>
 </table>
+<?php
+
+
+  include_once('ArrayCaseReverse.php');  
+  include_once('ArrayStringFunctions.php');
+  include_once('Book.php');  
+  include_once('CaseReverseFacade.php');
+
+
+
+
+ 
+  $book = 
+    new Book("Design Patterns",
+	            "Gamma, Helm, Johnson, and Vlissides");
+
+
+  echo "Original Product title: ".$book->getTitle();
+  echo tagins("br").tagins("br");
+
+
+  $bookTitleReversed = 
+    CaseReverseFacade::reverseStringCase($book->getTitle());  
+  
+  echo "Reversed book title: ".$bookTitleReversed;
+  echo tagins("br").tagins("br");
+
+
+  //doing this so code can be displayed without breaks
+  function tagins($stuffing) {
+    return "<".$stuffing.">";
+  }

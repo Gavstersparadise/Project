4a475e27ca34084e44042fd0be75881446f6e99b
Gavstersparadise <gavin_murphy1@hotmail.com>
2015-04-08 17:42:29 +0100

    Paypal template design pattern

--- application/controllers/Cust.php
+++ application/controllers/Cust.php
@@ -32,6 +32,30 @@ class Cust extends CI_Controller {
 		if ($this -> form_validation -> run() == FALSE) {
 			$this -> load -> view('shop/user_details');
 		} else {
+			
+	
+
+			$config['business'] = 'gavin_murphy1@hotmail.com';
+			$config['cpp_header_image'] = '';
+			//Image header url [750 pixels wide by 90 pixels high]
+			$config['return'] = 'http://localhost:8666/Project/index.php/site/members';
+			$config['cancel_return'] = 'http://localhost:8666/Project/index.php/site/members';
+			$config['notify_url'] = 'process_payment.php';
+			//IPN Post
+			$config['production'] = FALSE;
+			//Its false by default and will use sandbox
+			$config["invoice"] = random_string('numeric', 8);
+			//The invoice id
+
+			$this -> load -> library('paypal', $config); 
+
+			#$this->paypal->add(<name>,<price>,<quantity>[Default 1],<code>[Optional]);
+
+			$this -> paypal -> add('Student Enrolment', 100);
+			//Second item
+
+			$this -> paypal -> pay();
+			//Proccess the payment
 			$cust_data = array('cust_first_name' => $this -> input -> post('cust_first_name'), 'cust_last_name' => $this -> input -> post('cust_last_name'), 'cust_email' => $this -> input -> post('email'), 'cust_address' => $this -> input -> post('payment_address'), 'cust_created_at' => time());
 
 			$payment_code = mt_rand();
--- application/views/shop/user_details.php
+++ application/views/shop/user_details.php
@@ -6,7 +6,13 @@
         <?php echo form_input(array('name' => 'email', 'placeholder' => 'Email Address', 'maxlength' => '255', 'size' => '50')); ?><br />
         <?php echo form_input(array('name' => 'email_confirm', 'placeholder' => 'Confirm Email', 'maxlength' => '255', 'size' => '50')); ?><br />
         <?php echo form_textarea(array('name' => 'payment_address', 'placeholder' => 'Payment Address', 'rows' => '6', 'cols' => '40', 'size' => '50')); ?><br />
-        <?php echo form_submit('', 'Enter') ; ?><br />
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="EYEHUNCPAYVBE">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+					
         <?php echo form_close() ; ?>
     </form>
 </body>
